ac17999e1b27268249cb57da22181f0950e8d88d
Kimberly S <kimberlys99@icloud.com>
2021-01-13 00:55:22 +0100

    Set up Application
    - Create a new Application in Visual Studio Code
    - Created a header function
    - Created a begin for the main menu

---
+++ Learn_Spanish/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Learn_Spanish {
+    public class Program {
+        static void Main(string[] args) {
+            MainMenu();
+        }
+
+        // Header
+        public static void Header() {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("* * * * * * * * * * * * * * * * * * * * * * * *");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("* * * * * * * *  SPAANS LEREN   * * * * * * * *");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("* * * * * * * * * * * * * * * * * * * * * * * *\n");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        // Main menu 
+        static void MainMenu() {
+            Header();
+            Console.WriteLine("¡Hola! ¿Qué tal?\n\nIn deze applicatie kan je Spaanse woorden oefenen!\n\nKies een onderdeel:");
+        }
+    }
+}
